bc19665abb2c28d3a7145bcce42d6a9f918592b9
Alex Shomin <alex.sh0min@yandex.ru>
2024-12-04 02:57:51 +0700

    docker is set up (almost)

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --no-input
-#cp -r /app/static/. /backend_static/static/
+cp -r /app/collected_static/. /backend_static/static/
 gunicorn --bind 0.0.0.0:8000 foodgram_main.wsgi
