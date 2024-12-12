6b7cc601d14a0bc991693aa2199cbfd8222b5956
Alex Shomin <alex.sh0min@yandex.ru>
2024-12-12 19:15:04 +0700

    save progress

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from recipes.models import Ingredient, FavoriteRecipe, Recipe, Tag, Follow
-from api.serializers import (FollowSerializer,
-    IngredientsSerializer, FavoriteRecipeSerializer,
-    RecipeSerializer, TagsSerializer)
+from recipes.models import Ingredient, FavoriteRecipe, Recipe, Tag
+from api.serializers import (
+    IngredientsSerializer,
+    FavoriteRecipeSerializer,
+    RecipeSerializer,
+    TagsSerializer
+)
+from http import HTTPStatus
+
 # AbstractUserSerializer, FollowSerializer
+
+HTTP_BAD_REQUEST = HTTPStatus.BAD_REQUEST
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Рецепты."""
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         """Создание рецепта, привязка автора."""
+
         serializer.save(author=self.request.user)
 
     @action(detail=True, methods=['post', 'delete'], url_path='favorite')
@@ -26,26 +34,28 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Добавление рецепта в избранное."""
         recipe = get_object_or_404(Recipe, pk=pk)
         user = request.user
+        FAVORITE_RECIPE_EXISTS = FavoriteRecipe.objects.filter(
+            user=user,
+            recipe=recipe
+        ).exists()
 
         if request.method == 'POST':
-            if FavoriteRecipe.objects.filter(user=user, recipe=recipe
-                                             ).exists():
+            if FAVORITE_RECIPE_EXISTS:
                 return Response(
                     {'detail': 'Вы уже добавили этот рецепт в избранное.'},
-                    status=status.HTTP_400_BAD_REQUEST
+                    status=HTTP_BAD_REQUEST
                 )
             FavoriteRecipe.objects.get_or_create(user=user, recipe=recipe)
             serializer = FavoriteRecipeSerializer(recipe)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         elif request.method == 'DELETE':
-            # Удаление рецепта из избранного
-            favorite_recipe = get_object_or_404(
-                FavoriteRecipe,
-                user=user,
-                recipe=recipe
-            )
-            favorite_recipe.delete()
+            if not FAVORITE_RECIPE_EXISTS:
+                return Response(
+                    {'detail': 'Вы уже удалили рецепт из избранного.'},
+                    status=HTTP_BAD_REQUEST
+                )
+            FavoriteRecipe.objects.filter(user=user, recipe=recipe).delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -59,44 +69,3 @@ class TagViewSet(viewsets.ModelViewSet):
     """Теги."""
     queryset = Tag.objects.all()
     serializer_class = TagsSerializer
-
-
-class SubscribeViewSet(viewsets.ModelViewSet):
-    """Подписки."""
-    serializer_class = FollowSerializer
-    queryset = Follow.objects.all()
-
-    @action(detail=True, methods=['post', 'delete'], url_path='subscribe')
-    def subscribe(self, request, pk=None):
-        print("Метод subscribe вызван")
-        user_to_subscribe = self.get_object()
-        if request.method == 'POST':
-            print('test')
-            print(request.user, 'и' ,user_to_subscribe)
-            if request.user == user_to_subscribe:
-                return Response(
-                    {'error': 'Вы не можете подписаться на себя'},
-                    status=status.HTTP_400_BAD_REQUEST)
-            Follow.objects.get_or_create(
-                user=request.user, followed_user=user_to_subscribe)
-            return Response({'status': 'subscribed'})
-        elif request.method == 'DELETE':
-            # Logic to delete a subscription
-            Follow.objects.filter(
-                user=request.user, followed_user=user_to_subscribe).delete()
-            return Response({'status': 'unsubscribed'})
-
-'''
-    subscribe.mapping.delete
-    def unsubscribe(self, request, pk):
-        user = request.user
-        author = get_object_or_404(AbstractUser, id=pk)
-
-        if not user.follower.filter(author=author).exists():
-            return Response(
-                {'errors': 'Вы не подписаны на этого автора'},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        subscription = get_object_or_404(Follow, user=user, author=author)
-        subscription.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)'''
